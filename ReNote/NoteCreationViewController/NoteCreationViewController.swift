400832fcfcd0de4cf4c80832a039eb3aa7d9f669
Vlad Eliseev <freshymanchan@gmail.com>
2019-10-13 21:05:57 +1000

    Note Creation View created

---
+++ ReNote/NoteCreationViewController/NoteCreationViewController.swift
@@ -0,0 +1,31 @@
+//
+//  NoteCreationViewController.swift
+//  ReNote
+//
+//  Created by Vlad Eliseev on 13.10.2019.
+//  Copyright © 2019 Vlad Eliseev. All rights reserved.
+//
+
+import UIKit
+
+class NoteCreationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Creating a note"
+        view.backgroundColor = .white
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
