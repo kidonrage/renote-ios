1742d22324e465276169daaa5c3c97047a4a21fe
Vlad Eliseev <freshymanchan@gmail.com>
2019-10-13 18:29:38 +1000

    Start button added

--- ReNote/NotesListViewController.swift
+++ ReNote/NotesListViewController.swift
@@ -14,13 +14,26 @@ class NotesListViewController: UIViewController {
     private let notesTableView = NotesTableView()
     private let addButton: UIButton = {
         let button = UIButton()
+        
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("+", for: .normal)
-        button.backgroundColor = UIColor(red: 255 / 255.0, green: 226 / 255.0, blue: 7 / 255.0, alpha: 1)
+        button.backgroundColor = .backgroundYellow
         button.layer.cornerRadius = 30
         button.titleLabel?.font = UIFont.systemFont(ofSize: 40)
-        button.tintColor = .black
         button.setTitleColor(.black, for: .normal)
+        
+        return button
+    }()
+    private let startButton: UIButton = {
+        let button = UIButton()
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Play", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 40)
+        button.backgroundColor = .backgroundYellow
+        button.layer.cornerRadius = 30
+        
         return button
     }()
     
@@ -41,6 +54,7 @@ class NotesListViewController: UIViewController {
         view.addSubview(categoriesCollectionView)
         view.addSubview(notesTableView)
         view.addSubview(addButton)
+        view.addSubview(startButton)
         
         NSLayoutConstraint.activate([
             categoriesCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
@@ -56,7 +70,11 @@ class NotesListViewController: UIViewController {
             addButton.widthAnchor.constraint(equalToConstant: 60),
             addButton.heightAnchor.constraint(equalToConstant: 60),
             addButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
-            addButton.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -10)
+            addButton.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -10),
+            
+            startButton.heightAnchor.constraint(equalTo: addButton.heightAnchor),
+            startButton.centerYAnchor.constraint(equalTo: addButton.centerYAnchor),
+            startButton.rightAnchor.constraint(equalTo: addButton.leftAnchor, constant: -10)
         ])
     }
 
