642dbe30f66a0c8970bf2018a0912bb7a20d9402
Vlad Eliseev <freshymanchan@gmail.com>
2019-10-20 20:51:48 +1000

    AddCategory function added

--- ReNote/NoteCreationViewController/CategorySelectionView.swift
+++ ReNote/NoteCreationViewController/CategorySelectionView.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import ReNote_Core
 
 class CategorySelectionView: UIView {
+    
+    let storage = Storage()
     
     let header = SectionHeaderLabel(text: "Category")
     
@@ -18,6 +21,7 @@ class CategorySelectionView: UIView {
         button.setTitle("+ Add new", for: .normal)
         button.setTitleColor(.black, for: .normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        button.addTarget(self, action: #selector(addCategory), for: .touchUpInside)
         return button
     }()
     
@@ -30,13 +34,31 @@ class CategorySelectionView: UIView {
         
         heightAnchor.constraint(equalToConstant: 120).isActive = true
         
-        categoriesList.setCategories(Category.fetchCategories())
+        categoriesList.setCategories(storage.getCategories())
         
         setupUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func addCategory() {
+        let ac = UIAlertController(title: "Add Category", message: "Please enter a category name:", preferredStyle: .alert)
+        ac.addTextField { (textField) in
+            textField.placeholder = "Category Name"
+        }
+        ac.addAction(UIAlertAction(title: "Add", style: .default, handler: { (action) in
+            guard let nameTextField = ac.textFields?[0] else {return}
+            guard let name = nameTextField.text, name != "" else {return}
+            
+            let newCategory = self.storage.addCategory(name: name)
+            
+            self.categoriesList.addCategory(newCategory)
+        }))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        window?.rootViewController?.present(ac, animated: true, completion: nil)
     }
     
     private func setupUI() {
